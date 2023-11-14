3e5519f790de43f7454aec1083321f560eed1d12
Mukola <koliunia2006@gmail.com>
2023-11-14 19:49:00 +0200

    added a weather card

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import { GlobalStyle } from "../GlobalStyle";
 import { Hero } from "./Hero/Hero";
 import { HeroAPI } from "./Hero/HeroAPI";
 import {Footer} from './Footer/Footer';
+import Weather from './Weather_Card/WeatherCard'
 
 export const App = () => {
   return (
     <div>
       <Hero/>
+      <Weather/>
       <Footer/>
       <GlobalStyle/>
     </div>
